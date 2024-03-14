a7ab7e57180917c3371fff3c085eefa7d1d0acf5
Harsha-Devraj <145648999+Harsha-Devraj@users.noreply.github.com>
2024-03-14 08:24:34 +0530

    Update prg19.java

--- prg19.java
+++ prg19.java
@@ -11,23 +11,9 @@ System.setProperty("webdriver.chrome.driver",
 Testing\\demo\\src\\drivers\\chromedriver.exe");
  
 WebDriver driver=new ChromeDriver();
-//driver.manage().window().maximize();
+
 driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
-//alert box
-/*driver.findElement(By.id("alertBox")).click();
-Alert simpleAlert = driver.switchTo().alert();
-System.out.println(simpleAlert.getText());
-Thread.sleep(1000);
-simpleAlert.accept();
-System.out.println(driver.findElement(By.id("output")).getText());*/
-//confirm box
- 
-/*driver.findElement(By.id("confirmBox")).click();
- 
-Alert confirmationAlert=driver.switchTo().alert();
- 
-System.out.println(confirmationAlert.getText());*/
-// prompt box
+x
  
 driver.findElement(By.id("promptBox")).click();
  
