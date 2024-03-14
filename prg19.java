721824981d90b6da465f18156a85f053c3addba2
Harsha-Devraj <145648999+Harsha-Devraj@users.noreply.github.com>
2024-03-14 08:23:38 +0530

    Update prg19.java

--- prg19.java
+++ prg19.java
@@ -1,42 +1,46 @@
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class App19 {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Users/shreyasgs/Documents/chromedriver-mac-arm64/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        // WebDriverWait wait = new WebDriverWait(driver, 3);
-
-        driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
-        //
-        driver.findElement(By.id("alertBox")).click();       
-        Alert simpleAlert = driver.switchTo().alert();
-        System.out.println(simpleAlert.getText());
-        Thread.sleep(2000);
-        simpleAlert.accept();
-        Thread.sleep(2000);
-       //
-        driver.findElement(By.id("confirmBox")).click();
-        Alert confirmAlert = driver.switchTo().alert();
-        System.out.println(confirmAlert.getText());
-        Thread.sleep(2000);
-        confirmAlert.dismiss();
-        Thread.sleep(2000);
-        System.out.println(driver.findElement(By.id("output")).getText());
-        //
-        driver.findElement(By.id("promptBox")).click();
-        Alert promptAlert = driver.switchTo().alert();
-        System.out.println(promptAlert.getText());
-        promptAlert.sendKeys("Piggy Naidu");
-        Thread.sleep(2000);
-        promptAlert.accept();
-        Thread.sleep(2000);
-        System.out.println(driver.findElement(By.id("output")).getText());
-
-        driver.quit();
-        
-    }
+public class prg_7_19 {
+ 
+public static void main(String[] args) throws Exception {
+ 
+System.setProperty("webdriver.chrome.driver", 
+"C:\\Users\\saish\\OneDrive\\Desktop\\computer science\\javapro\\Software 
+Testing\\demo\\src\\drivers\\chromedriver.exe");
+ 
+WebDriver driver=new ChromeDriver();
+//driver.manage().window().maximize();
+driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
+//alert box
+/*driver.findElement(By.id("alertBox")).click();
+Alert simpleAlert = driver.switchTo().alert();
+System.out.println(simpleAlert.getText());
+Thread.sleep(1000);
+simpleAlert.accept();
+System.out.println(driver.findElement(By.id("output")).getText());*/
+//confirm box
+ 
+/*driver.findElement(By.id("confirmBox")).click();
+ 
+Alert confirmationAlert=driver.switchTo().alert();
+ 
+System.out.println(confirmationAlert.getText());*/
+// prompt box
+ 
+driver.findElement(By.id("promptBox")).click();
+ 
+Alert promptAlert=driver.switchTo().alert();
+ 
+System.out.println(promptAlert.getText());
+ 
+promptAlert.sendKeys(" ");
+ 
+promptAlert.accept();
+ 
+System.out.println(driver.findElement(By.id("output")).getText());
+ 
+Thread.sleep(2000);
+}
 }
